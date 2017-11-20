cf15441c27d56f3bd6ba576b05f16e866e344c3d
KillzXGaming <millernathan35@gmail.com>
2017-11-19 20:07:10 -0500

    Add more path types (WIP)

--- addon_add_object.py
+++ addon_add_object.py
@@ -37,9 +37,94 @@ from bpy.types import (Panel,
                        )
 
 
+class OBJECT_OT_HEMI(bpy.types.Operator):
+    bl_idname = "my.hemisphere"
+    bl_label = "hemisphere"
+
+ 
+    def execute(self, context):				   
+        lamp_data = bpy.data.lamp.new(name="hemisphere",type='HEMI')
+        lamp_object = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)			   
+        scene.objects.link(lamp_object)
+        lamp_object.location = (0, 0, 0)					   
+
+        return{'FINISHED'}
+
+class MySettings(PropertyGroup):
+
+    my_bool = BoolProperty(
+        name="Enable or Disable",
+        description="A bool property",
+        default = False
+        )
+
+    my_enum = EnumProperty(
+        name="Dropdown:",
+        description="Apply Data to attribute.",
+        items=[ ('OP1', "Option 1", ""),
+                ('OP2', "Option 2", ""),
+                ('OP3', "Option 3", ""),
+               ]
+        )
+
+    my_enum2 = EnumProperty(
+        name="Dropdown2:",
+        description="Apply Data to attribute.",
+        items=[ ('OP1', "Lap Path", ""),
+                ('OP2', "Enemy Path", ""),
+                ('OP3', "Gravity Path", ""),
+               ]
+        )
+
+class ObjectPanel(bpy.types.Panel):
+    bl_label = "Hello from Object context"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+ 
+    def draw(self, context):
+        scene = context.scene
+        obj = context.object
+        layout = self.layout
+        layout.label("First row")
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row = layout.row()
+        row.operator("object.lamp_add" , text="Add Lamp", icon='LAMP_AREA')
+        layout.prop(obj, "my_bool") 
+        layout.prop(obj, "my_enum", text="") 
+		
+class PathSETTINGS(Panel):
+    bl_idname = "OBJECT_PT_my_panel"
+    bl_label = "My Panel"
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "TOOLS"    
+    bl_category = "MK8"
 
 
+    @classmethod
+    def poll(self,context):
+        return context.object is not None
+
+    def draw(self, context):
+        obj = context.object
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
+        layout.label("First row")
+        row = layout.row(align=True)
+        row.alignment = 'EXPAND'
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add" , text="Cube")
+       # row.operator("object.lamp_add(type='HEMI'), text="Add Hemisphere")
+        row = layout.row()
+        layout.prop(mytool, "my_enum2", text="") 
 	
+#Lap Paths
 def add_object(self, context):
 
 
@@ -61,14 +146,54 @@ def add_object(self, context):
     # mesh.validate(verbose=True)
     object_data_add(context, mesh, operator=self)
 
+#Gravity Paths
+def add_object1(self, context):
 
+
+    verts = [Vector((-1.0, 0, -1.0)),
+             Vector((-1.0, 0, 1.0)), 
+             Vector((1.0, 0, -1.0)), 
+             Vector((1.0, 0, 1.0)), 
+             Vector((-1.0, 0.01, -1.0)), 
+             Vector((-1.0, 0.01, 1.0)), 
+             Vector((1.0, 0.01, -1.0)), 
+             Vector((1.0, 0.01, 1.0))]
+    
+    edges = []
+    faces = [(2, 3, 1, 0), (6, 7, 5, 4), (7, 3, 1, 5)]
+
+    mesh = bpy.data.meshes.new(name="Gravity Path")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
 			
-			
+#Glider Paths
+def add_object2(self, context):
+
+
+    verts = [Vector((-1.0, 0, -1.0)),
+             Vector((-1.0, 0, 1.0)), 
+             Vector((1.0, 0, -1.0)), 
+             Vector((1.0, 0, 1.0)), 
+             Vector((-1.0, 0.01, -1.0)), 
+             Vector((-1.0, 0.01, 1.0)), 
+             Vector((1.0, 0.01, -1.0)), 
+             Vector((1.0, 0.01, 1.0))]
+    
+    edges = []
+    faces = [(2, 3, 1, 0), (6, 7, 5, 4), (7, 3, 1, 5)]
+
+    mesh = bpy.data.meshes.new(name="Glide Path")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)		
 	
 
 class Add_LapPath(Operator, AddObjectHelper):
     """Create a new Mesh Object"""
-    bl_idname = "mesh.add_object"
+    bl_idname = "mesh.add_lap"
     bl_label = "Lap Path"
     bl_options = {'REGISTER', 'UNDO'}
 
@@ -127,9 +252,127 @@ class Add_LapPath(Operator, AddObjectHelper):
     
         return {'FINISHED'}
 	
+class Add_GravityPath(Operator, AddObjectHelper):
+    """Create a new Mesh Object"""
+    bl_idname = "mesh.add_gravity"
+    bl_label = "Gravity Path"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale = FloatVectorProperty(
+            name="scale",
+            default=(0.25, 0.25, 0.5),
+            subtype='TRANSLATION',
+            description="scaling",
+            )
+    
 
 
-	
+
+    def execute(self, context):
+        
+        add_object1(self, context)
+		
+        scene = context.scene
+        obj = scene.objects.active
+		
+        activeObject = scene.objects.active #Set active object to variable
+        mat = bpy.data.materials.new(name="Front1") #set new material to variable
+        activeObject.data.materials.append(mat) #add the material to the object
+        bpy.context.object.active_material.diffuse_color = (1, 0.006883, 0) #Color Red 
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+        activeObject.data.polygons[1].select = True  #Active polygon
+        
+        me = obj.data
+        
+        for f in me.polygons:
+         f.material_index = f.index % 3
+
+        mat1 = bpy.data.materials.new(name="Back1") #set new material to variable
+        activeObject.data.materials.append(mat1) #add the material to the object  
+        bpy.context.object.active_material_index = 1 #Active the second material
+        bpy.context.object.active_material.diffuse_color = (0.225547,0, 1) #Color Green
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+		
+        activeObject.data.polygons[2].select = True  #Active polygon
+        
+        me = obj.data
+        
+        for f in me.polygons:
+         f.material_index = f.index % 3
+
+        mat1 = bpy.data.materials.new(name="Back") #set new material to variable
+        activeObject.data.materials.append(mat1) #add the material to the object  
+        bpy.context.object.active_material_index = 1 #Active the second material
+        bpy.context.object.active_material.diffuse_color = (0.225547,0, 1) #Color Green
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+        
+        bpy.ops.transform.resize(value=(0.25, 0.25, 0.25))
+    
+        return {'FINISHED'}
+
+class Add_GlidePath(Operator, AddObjectHelper):
+    """Create a new Mesh Object"""
+    bl_idname = "mesh.add_glide"
+    bl_label = "Glide Path"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale = FloatVectorProperty(
+            name="scale",
+            default=(0.25, 0.25, 0.5),
+            subtype='TRANSLATION',
+            description="scaling",
+            )
+    
+
+
+
+    def execute(self, context):
+        
+        add_object2(self, context)
+		
+        scene = context.scene
+        obj = scene.objects.active
+		
+        activeObject = scene.objects.active #Set active object to variable
+        mat = bpy.data.materials.new(name="Front") #set new material to variable
+        activeObject.data.materials.append(mat) #add the material to the object
+        bpy.context.object.active_material.diffuse_color = (1, 0.006883, 0) #Color Red 
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+        activeObject.data.polygons[1].select = True  #Active polygon
+        
+        me = obj.data
+        
+        for f in me.polygons:
+         f.material_index = f.index % 3
+
+        mat1 = bpy.data.materials.new(name="Back") #set new material to variable
+        activeObject.data.materials.append(mat1) #add the material to the object  
+        bpy.context.object.active_material_index = 1 #Active the second material
+        bpy.context.object.active_material.diffuse_color = (1,0.311029, 0) #Color Orange
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+		
+        activeObject.data.polygons[2].select = True  #Active polygon
+        
+        me = obj.data
+        
+        for f in me.polygons:
+         f.material_index = f.index % 3
+
+        mat1 = bpy.data.materials.new(name="Back") #set new material to variable
+        activeObject.data.materials.append(mat1) #add the material to the object  
+        bpy.context.object.active_material_index = 1 #Active the second material
+        bpy.context.object.active_material.diffuse_color = (1,0.311029, 0) #Color Orange
+        bpy.context.object.active_material.use_transparency = True
+        bpy.context.object.active_material.alpha = (0.4) #Transparent Amount
+        
+        bpy.ops.transform.resize(value=(0.25, 0.25, 0.25))
+    
+        return {'FINISHED'}	
 
 
 
@@ -147,8 +390,16 @@ def add_object_button(self, context):
         Add_LapPath.bl_idname,
         text="Lap Path",
         icon='PLUGIN')
-
-
+    self.layout.operator(
+        Add_GravityPath.bl_idname,
+        text="Gravity Path",
+        icon='PLUGIN')
+    self.layout.operator(
+        Add_GlidePath.bl_idname,
+        text="Glide Path",
+        icon='PLUGIN')
+		
+		
 # This allows you to right click on a button and link to the manual
 def add_object_manual_map():
     url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
@@ -168,7 +419,12 @@ def register():
     bpy.types.INFO_MT_mesh_add.append(add_object_button)
     bpy.utils.register_class(ExportSomeData)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
-
+    bpy.utils.register_class(Add_GravityPath)
+    bpy.utils.register_class(Add_GlidePath)
+    bpy.utils.register_class(OBJECT_OT_HEMI)
+    bpy.utils.register_class(ObjectPanel)
+    bpy.utils.register_class(MySettings)
+    bpy.utils.register_class(PathSETTINGS)
 
     
 
@@ -179,6 +435,12 @@ def unregister():
     bpy.types.INFO_MT_mesh_add.remove(add_object_button)
     bpy.utils.unregister_class(ExportSomeData)
     bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    bpy.utils.unregister_class(Add_GravityPath)
+    bpy.utils.unregister_class(Add_GlidePath)
+    bpy.utils.unregister_class(OBJECT_OT_HEMI)
+    bpy.utils.unregister_class(ObjectPanel)
+    bpy.utils.unregister_class(MySettings)
+    bpy.utils.unregister_class(PathSETTINGS)
 
     
 
